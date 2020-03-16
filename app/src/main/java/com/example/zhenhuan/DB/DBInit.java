3657d912c424cb5734e008fcd64d5776877308eb
panpenggen <panpenggen@hzsuidifu.com>
2020-03-16 11:43:28 +0800

    attribute testdata

--- app/src/main/java/com/example/zhenhuan/DB/DBInit.java
+++ app/src/main/java/com/example/zhenhuan/DB/DBInit.java
@@ -3,14 +3,17 @@ package com.example.zhenhuan.DB;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
+import java.lang.reflect.Type;
 import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DBInit extends SQLiteOpenHelper {
     // 数据库默认名字
-    public static final String db_name = "zhenhuan.db";
+    public static final String db_name = "zhenhuan_db";
+    SQLiteDatabase db;
 
     public DBInit(Context context, int version) {
         super(context, db_name, null, version);
@@ -18,7 +21,11 @@ public class DBInit extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        createTable(db);
+        Log.i("sql", "createTable: ");
+        String attribute_table = "create table attribute(id integer primary key autoincrement,name vchar,value int)";
+        db.execSQL(attribute_table);
+
+//        addtestdata(db);
     }
 
     @Override
@@ -26,9 +33,7 @@ public class DBInit extends SQLiteOpenHelper {
     }
 
 
-    private void createTable(SQLiteDatabase db) {
-        String attribute_table = "create table attribute(id integer primary key autoincrement,name vchar,value int)";
-        db.execSQL(attribute_table);
+    public void addtestdata(SQLiteDatabase db){
         Map<String,Integer> map_attribute = new HashMap<String,Integer>();
         map_attribute.put("健康",80);
         map_attribute.put("魅力",60);
@@ -37,11 +42,14 @@ public class DBInit extends SQLiteOpenHelper {
         for(Map.Entry<String,Integer> entry: map_attribute.entrySet()){
             String mapKey = entry.getKey();
             int mapValue = entry.getValue();
-            String attribute_sql= MessageFormat.format("insert into attribute(name,value) values('{0}','{1}')", mapKey,mapValue);
-            db.execSQL(attribute_sql);
+            Log.i("sql", "insert into attribute");
+            Object objects[] = new Object[2];
+            objects[0] = mapKey;
+            objects[1] = mapValue;
+            db.execSQL("insert into attribute(name,value) values('?','?')", objects);
         }
-
     }
+
 
 
 
--- app/src/main/java/com/example/zhenhuan/MainActivity.java
+++ app/src/main/java/com/example/zhenhuan/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.zhenhuan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.amitshekhar.DebugDB;
+import com.example.zhenhuan.DB.DBInit;
+
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
+import com.example.zhenhuan.DB.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //显示数据库数据
+        DebugDB.getAddressLog();
+
+        DBInit dbInit = new DBInit(this,1);
+        SQLiteDatabase db = dbInit.getWritableDatabase();
+        add_testdata(db);
 
         text = (TextView) findViewById(R.id.text);
         button_nextyear = (Button) findViewById(R.id.button_nextyear);
@@ -49,6 +67,29 @@ public class MainActivity extends AppCompatActivity {
         attribute_value_list.add(attribute_value_1);
         attribute_value_list.add(attribute_value_2);
         attribute_value_list.add(attribute_value_3);
+
+    }
+
+
+    public void add_testdata(SQLiteDatabase db){
+        ContentValues values = new ContentValues();
+
+        Map<String,Integer> map_attribute = new HashMap<String,Integer>();
+        map_attribute.put("健康",80);
+        map_attribute.put("魅力",60);
+        map_attribute.put("学识",10);
+
+        Log.i("sql", "add_testdata: ");
+
+        for(Map.Entry<String,Integer> entry: map_attribute.entrySet()){
+            String mapKey = entry.getKey();
+            int mapValue = entry.getValue();
+            Log.i("sql", "insert into attribute:"+mapKey+" "+mapValue);
+            values.put("name",mapKey);
+            values.put("value",mapValue);
+            db.insert("attribute", null, values);
+        }
+
 
     }
 
